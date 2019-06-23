0a2221f8677189c9fdc3e3f7e1533188857cd2d3
Taeim Kwon <k.taemee@gmail.com>
2019-06-23 18:42:39 +0100

    added controller logic

--- address-book-api/src/main/java/com/moo/taeim/addressbook/controller/AddressController.java
+++ address-book-api/src/main/java/com/moo/taeim/addressbook/controller/AddressController.java
@@ -2,6 +2,7 @@ package com.moo.taeim.addressbook.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,23 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.moo.taeim.addressbook.model.User;
 import com.moo.taeim.addressbook.model.UserAddress;
+import com.moo.taeim.addressbook.service.UserAddressService;
 
 @RestController
 public class AddressController {
+	
+    @Autowired
+    private UserAddressService userAddressService;
 
     @GetMapping("/addresses")
     public ResponseEntity<List<User>> getUsers(@RequestParam(value = "surname", required = false) String surname) {
-
  
-        return null;
+    	List<User> searchResultlist = userAddressService.getUserList(surname);
+
+        return ResponseEntity.ok(searchResultlist);
     }
 
     @GetMapping(value = "/addresses/{userNumber}")
     public ResponseEntity<UserAddress> getUserDetail(@PathVariable("userNumber") int userNumber) {
 
-   
+        UserAddress searchResult = userAddressService.getUserAddress(userNumber);
 
-        return null;
+        return ResponseEntity.ok(searchResult);
     }
 
 }
